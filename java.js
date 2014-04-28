9e1fefc899ffd10cbb28a069b4681643595e8a08
AnUbHaVag <anubhavagarwal033@yahoo.com>
2014-04-28 16:40:22 +0800

    Update java.js

--- java.js
+++ java.js
@@ -6,7 +6,8 @@ id.style.backgroundColor = ""+color+"";
 setTimeout(doIt,50)
 function motota() {
 var namru = document.getElementById('damack').value;
+var namrnam = document.getElementById('damack2').value;
 var div = document.getElementById('chicho');
-div.innerHTML = ""+namru+"";
+div.innerHTML = "<style>"+namrnam+"</style>"+namru+"";
 alert('Your code has loaded.')
 };
