0cdb558cb12d6a7d5f40a7587d26717585e83f6b
AnUbHaVag <anubhavagarwal033@yahoo.com>
2014-04-28 15:40:06 +0800

    Create java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+function doIt(){
+var color = "red";
+var id = document.getElementById('body');
+id.style.backgroundColor = ""+color+"";  
+}
+setTimeout(doIt,50)
+function motota() {
+var namru = document.getElementById('damack').value;
+var div = document.getElementById('chicho');
+div.innerHTML = ""+namru+"";
+alert('Your code has loaded.')
+};
