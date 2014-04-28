02118251c48b6768fa58d65b6323afb517769c81
AnUbHaVag <anubhavagarwal033@yahoo.com>
2014-04-28 14:46:05 +0800

    MAIN FILE ADDED
    j

---
+++ java.js
@@ -0,0 +1,6 @@
+function motota() {
+var namru = document.getElementById('damack').value;
+var div = document.getElementById('chicho');
+div.innerHTML = ""+namru+"";
+alert('Your code has loaded.')
+};
